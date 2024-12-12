c7545c669f1f9e9b95275a94def49e7e57536fcd
oybek003 <ojbekmuslimov1@gmail.com>
2024-12-12 15:41:37 +0500

    Add files via upload

---
+++ skript.js
@@ -0,0 +1,11 @@
+let userName = prompt('your name:');
+
+let surName = prompt('your surname:');
+
+let userAge = prompt('your age:');
+
+let useIT = prompt('your IT:');
+
+let useLocation = prompt('where do you live:');
+
+alert(`Thank you \n ${userName}`);
